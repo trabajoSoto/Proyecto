55b8072ce229418baf63584d0a78476f55c221eb
trabajoSoto <79877848+trabajoSoto@users.noreply.github.com>
2021-03-04 00:44:10 +0100

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<div class="dashboard">
+	<div class="sidebar" style="width:25vw; display:inline-block;">
+		<ul>
+		<div><a  href='index.php?action=show-customer'>MOSTRAR TODOS LOS SOCIOS </a></div>
+		<div><a  href='index.php?action=show-employee'>MOSTRAR ACTIVIDADES DE LOS EMPLEADOS </a></div>
+		<div><a  href='index.php?action=show-schedule'>MOSTRAR HORARIO </a></div>
+		<div><a  href='index.php?action=search-employee'>BUSCA EMPLEADO </a></div>
+		<div><a  href='index.php?action=search-customer'>BUSCA SOCIO </a></div>
+		<div><a  href='index.php?action=show-reserve'>RESERVAS </a></div>
+		</ul>
+	</div>
+</div>
