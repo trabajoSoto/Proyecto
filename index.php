c6720efe326efedec2412a42fbc4df7c3a374c45
trabajoSoto <79877848+trabajoSoto@users.noreply.github.com>
2021-03-04 00:43:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link  rel="stylesheet" type="text/css">
+        <title>SPAKARMA</title>
+    </head>
+    <body class=fondo> 
+        
+        <?php 
+
+            require_once ('app.php');
+            $app = new App();
+            $app->run();
+        ?>
+    </body>
+</html>
