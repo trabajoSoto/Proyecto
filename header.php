fd61245dcc34e5f5325b875fa4e3a0002d08e548
trabajoSoto <79877848+trabajoSoto@users.noreply.github.com>
2021-03-04 00:44:45 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>SPAKARMA</title>
+</head>
+<body>
+	<p>SPAKARMA</p>
+	<p>Tonificación de cuerpo y mente</p>
+	<div style="display:flex; justify-content: space-between;">
+		<div><a  href='index.php?action=show-customer'>MOSTRAR TODOS LOS SOCIOS </a></div>
+		<div><a  href='index.php?action=show-employee'>MOSTRAR ACTIVIDADES DE LOS EMPLEADOS </a></div>
+		<div><a  href='index.php?action=show-schedule'>MOSTRAR HORARIO </a></div>
+		<div><a  href='index.php?action=search-employee'>BUSCA EMPLEADO </a></div>
+		<div><a  href='index.php?action=search-customer'>BUSCA SOCIO </a></div>
+		<div><a  href='index.php?action=show-reserve'>RESERVAS </a></div>
+	</div>
+</body>
